20ca742baff6f97c0b0f0b5658b755916c327f43
brady gaster <bradyg@microsoft.com>
2020-02-07 01:50:37 -0800

    direct communication working

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -1,3 +1,27 @@
+var map;
+
 function addPointToMap(user, lat, long) { 
-    console.log(user + ': ' + lat + ', ' + long);
+    console.log('adding user ' + user + ': ' + lat + ', ' + long);
+
+    var pushpin = new Microsoft.Maps.Pushpin(
+        new Microsoft.Maps.Location(parseFloat(lat), parseFloat(long)),
+        {
+            color: 'blue', 
+            title: user 
+        }
+    );
+
+    map.entities.push(pushpin);
+
+    console.log('added user ' + user);
+}
+
+function initMap() {
+    console.log('initializing map');
+
+    map = new Microsoft.Maps.Map(document.getElementById('map'), {
+        center: new Microsoft.Maps.Location(47.6149, -122.1941)
+    });
+
+    console.log('map initialized');
 }
